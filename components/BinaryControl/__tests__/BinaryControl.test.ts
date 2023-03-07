86eea66b169c0dc9a26b664c813850033cc2e41a
Andy Evans <andrew@aevans.me>
2023-03-07 23:40:09 +1300

    Final check-in

---
+++ components/BinaryControl/__tests__/BinaryControl.test.ts
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react-native'
+import BinaryControl from "../BinaryControl"
+
+it('should render the binary options on the page', async () => {
+    // TODO:... Unit Tests using RNTL
+})
+
+it('should toggle the selected option when the option is pressed', async () => {
+    // TODO:... Unit Tests using RNTL
+})
